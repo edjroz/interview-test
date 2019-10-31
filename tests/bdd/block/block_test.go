e4f8dad3faaa5d7e942c4890493b312e9dec7e10
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2019-10-31 16:21:39 -0400

    move context

--- tests/bdd/block/block_test.go
+++ tests/bdd/block/block_test.go
@@ -34,16 +34,16 @@ var _ = Describe("Block", func() {
 				Expect(err).ToNot(BeNil())
 				Expect(err.Error()).To(ContainSubstring(block.BlockByNumberError.Error()))
 			})
-			Context("Valid hash", func() {
-				It("Should return the ethereum genesis block", func() {
-					// NOTE: use a valid has hex address otherwise test will fail
-					hx := "0x6341fd3daf94b748c72ced5a5b26028f2474f5f00d824504e4fa37a75767e177"
-					res, err := block.GetBlockByHash(hx)
-					Expect(err).To(BeNil())
-					Expect(res).ToNot(BeNil())
-					Expect(res.Height).To(BeZero())
-					Expect(res.Time).ToNot(BeZero())
-				})
+		})
+		Context("Valid hash", func() {
+			It("Should return the ethereum genesis block", func() {
+				// NOTE: use a valid has hex address otherwise test will fail
+				hx := "0x6341fd3daf94b748c72ced5a5b26028f2474f5f00d824504e4fa37a75767e177"
+				res, err := block.GetBlockByHash(hx)
+				Expect(err).To(BeNil())
+				Expect(res).ToNot(BeNil())
+				Expect(res.Height).To(BeZero())
+				Expect(res.Time).ToNot(BeZero())
 			})
 		})
 	})
